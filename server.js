1fcae4057df5afe3f297c4c77e78d88f54a85dca
Neeraj <159523608+neeraj1240@users.noreply.github.com>
2024-11-16 11:38:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
-const wordsData = JSON.parse(fs.readFileSync('dictionary data\\dictionary.json', 'utf8'));
+const wordsData = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'));
 
 
 
